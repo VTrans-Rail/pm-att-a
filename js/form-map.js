2d4b9fc2155bffa0ddcb59747b6d62a254e64258
Stephen <stephen@mapsmith.net>
2016-03-14 10:25:48 -0400

    tweak basemap and center

--- js/form-map.js
+++ js/form-map.js
@@ -1,7 +1,7 @@
 require(["esri/map", "dojo/domReady!"], function(Map) {
   var map = new Map("map", {
-    center: [-118, 34.5],
-    zoom: 8,
-    basemap: "topo"
+    center: [-73.75, 44],
+    zoom: 7,
+    basemap: "topo-vector"
   });
 });
